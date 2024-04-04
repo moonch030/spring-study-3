e773574c37c708153052309b43a8e171b29f22f2
moonch <cogus2010@naver.com>
2024-04-04 11:22:39 +0900

    2024-04-04

--- ch04_pjt_01/src/main/java/ch04_pjt_01/ems/member/service/EMSInformationService.java
+++ ch04_pjt_01/src/main/java/ch04_pjt_01/ems/member/service/EMSInformationService.java
@@ -21,7 +21,152 @@ public class EMSInformationService {
     private Map<String, String> administrators;  // 관리자 목록
     private Map<String, DBConnectionInfo> dbInfos;  // DB 연결 정보 목록
     
-    // EMS 정보 출력 메서드
+    
+    public String getInfo() {
+		return info;
+	}
+
+
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+
+
+	public String getCopyRight() {
+		return copyRight;
+	}
+
+
+
+	public void setCopyRight(String copyRight) {
+		this.copyRight = copyRight;
+	}
+
+
+
+	public String getVer() {
+		return ver;
+	}
+
+
+
+	public void setVer(String ver) {
+		this.ver = ver;
+	}
+
+
+
+	public int getsYear() {
+		return sYear;
+	}
+
+
+
+	public void setsYear(int sYear) {
+		this.sYear = sYear;
+	}
+
+
+
+	public int getsMonth() {
+		return sMonth;
+	}
+
+
+
+	public void setsMonth(int sMonth) {
+		this.sMonth = sMonth;
+	}
+
+
+
+	public int getsDay() {
+		return sDay;
+	}
+
+
+
+	public void setsDay(int sDay) {
+		this.sDay = sDay;
+	}
+
+
+
+	public int geteYear() {
+		return eYear;
+	}
+
+
+
+	public void seteYear(int eYear) {
+		this.eYear = eYear;
+	}
+
+
+
+	public int geteMonth() {
+		return eMonth;
+	}
+
+
+
+	public void seteMonth(int eMonth) {
+		this.eMonth = eMonth;
+	}
+
+
+
+	public int geteDay() {
+		return eDay;
+	}
+
+
+
+	public void seteDay(int eDay) {
+		this.eDay = eDay;
+	}
+
+
+
+	public List<String> getDevelopers() {
+		return developers;
+	}
+
+
+
+	public void setDevelopers(List<String> developers) {
+		this.developers = developers;
+	}
+
+
+
+	public Map<String, String> getAdministrators() {
+		return administrators;
+	}
+
+
+
+	public void setAdministrators(Map<String, String> administrators) {
+		this.administrators = administrators;
+	}
+
+
+
+	public Map<String, DBConnectionInfo> getDbInfos() {
+		return dbInfos;
+	}
+
+
+
+	public void setDbInfos(Map<String, DBConnectionInfo> dbInfos) {
+		this.dbInfos = dbInfos;
+	}
+
+
+
+	// EMS 정보 출력 메서드
     public void printEMSInformation() {
         System.out.println("EMS INFORMATION START ----------");
         String devPeriod = sYear + "/" + sMonth + "/" + sDay + " ~ " + eYear + "/" + eMonth + "/" + eDay;
